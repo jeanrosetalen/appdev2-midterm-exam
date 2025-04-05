474346e20e14a9f6561dd4181913ee0aab021e19
rjean5097 <rjean5097@gmail.com>
2025-04-05 05:26:31 +0000

    Finished Practical Exam

--- 03_url.js
+++ 03_url.js
@@ -2,8 +2,6 @@ const url = require('url')
 const myUrl = 'https://www.example.com:8080/products?category=electronics&sort=price'
 const parsedUrl = url.parse(myUrl, true);
 
-console.log(parsedUrl)
-
 console.log('Protocol:', parsedUrl.protocol);
 console.log('Hostname:', parsedUrl.hostname);
 console.log('Port:', parsedUrl.port);
